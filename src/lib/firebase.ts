0ed4d38cb3a42a06f0ba059f40043517706aac5f
Tomek <tlyko94@gmail.com>
2025-12-29 14:24:43 +0100

    feat: update firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: "AIzaSyAQwhojpJHoSNI6dLPw9YWehuSeP2x-rzY",
+    authDomain: "impostor-709e6.firebaseapp.com",
+    databaseURL: "https://impostor-709e6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "impostor-709e6",
+    storageBucket: "impostor-709e6.firebasestorage.app",
+    messagingSenderId: "759389324366",
+    appId: "1:759389324366:web:8d398712fffa21f1832f1f",
+    measurementId: "G-X38J2EY20J"
 };
 
 // Initialize Firebase (singleton pattern)
